5115b49db78e11903c8f2c2bb145563ee5c129f9
Alejandro Alonso Mayo <alejandroalonsomayo@gmail.com>
2020-08-04 01:00:48 +0200

    feat: reads temperature input
    Closes #2

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,7 +1,14 @@
+#define TEMP_SENSOR_PORT 0
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Hello world!");
+  int portValue = analogRead(TEMP_SENSOR_PORT);
+  float voltaje = 5.0 / 1024 * portValue;
+  float temp = voltaje * 100 - 50;
+  Serial.print("Temp: ");
+  Serial.println(temp);
+  delay(1000);
 }
